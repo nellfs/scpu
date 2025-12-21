872394c5f7a5dbc79fd2c10d3fa5e4a04617be04
Heron Ximenes <pokycelit@gmail.com>
2025-12-21 20:12:55 -0300

    CPU execution trace

--- cmd/main.go
+++ cmd/main.go
@@ -19,9 +19,46 @@ func main() {
 	bus.Mem[0xFFFC] = 0x00
 	bus.Mem[0xFFFD] = 0x80
 
-	// Print the memory program
-	fmt.Printf("Program bytes:\n")
-	for i := uint16(0x8000); i <= 0x8003; i++ {
-		fmt.Printf("$%04X: 0x%02X\n", i, bus.Mem[i])
+	cpu := hardware.NewCpu(bus)
+	cpu.Reset()
+
+	fmt.Println("Starting CPU execution trace...")
+
+	for i := 0; i < 2; i++ {
+		pc := cpu.PC
+		opcode := bus.Mem[pc]
+
+		var value byte
+		switch opcode {
+		case 0x69: // Only for debug
+			value = bus.Mem[pc+1]
+		default:
+			value = 0
+		}
+
+		fmt.Printf("Before Step: PC: $%04X | Opcode: 0x%02X | A: %d | Adding: %d | C:%v Z:%v N:%v V:%v\n",
+			pc, opcode, cpu.A, value,
+			cpu.GetFlag(hardware.FlagC),
+			cpu.GetFlag(hardware.FlagZ),
+			cpu.GetFlag(hardware.FlagN),
+			cpu.GetFlag(hardware.FlagV))
+
+		cpu.Step()
+
+		fmt.Printf("After Step:  PC: $%04X | A: %d | C:%v Z:%v N:%v V:%v\n\n",
+			cpu.PC, cpu.A,
+			cpu.GetFlag(hardware.FlagC),
+			cpu.GetFlag(hardware.FlagZ),
+			cpu.GetFlag(hardware.FlagN),
+			cpu.GetFlag(hardware.FlagV))
 	}
+
+	// Print final state
+	fmt.Println("------")
+	fmt.Println("\nFinal CPU state:")
+	fmt.Printf("A: %d\n", cpu.A)
+	fmt.Printf("Carry flag: %v\n", cpu.GetFlag(hardware.FlagC))
+	fmt.Printf("Zero flag: %v\n", cpu.GetFlag(hardware.FlagZ))
+	fmt.Printf("Negative flag: %v\n", cpu.GetFlag(hardware.FlagN))
+	fmt.Printf("Overflow flag: %v\n", cpu.GetFlag(hardware.FlagV))
 }
